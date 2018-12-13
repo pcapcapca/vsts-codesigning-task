f05c375bcb8d2e824fa9e16a93c7454fa5b324bd
pcapcapca <pcapcapca@gmail.com>
2018-12-13 13:32:59 +0100

    Globbing support added

--- task/index.ts
+++ task/index.ts
@@ -2,6 +2,7 @@ import path = require("path");
 import tl = require("vsts-task-lib/task");
 import exec = require("child_process");
 import sec = require("./securefiledownloader");
+import glob = require("glob");
 
 async function run(): Promise<void> {
   let secureFileId: string;
@@ -23,27 +24,41 @@ async function run(): Promise<void> {
     secureFileHelpers = new sec.SecureFileDownloader();
     secureFilePath = await secureFileHelpers.downloadSecureFile(secureFileId);
 
-    console.log("Signing file");
-    var exePath: string = path.resolve(__dirname, "./signtool.exe");
-    console.log("Executing signtool at " + exePath);
-    exec.execFile(exePath, ["sign", "/fd", hashingAlgorithm, "/t", timeServer, "/f", secureFilePath, "/p", signCertPassword, filePath],
-      (err, data) => {
-        if (err) {
-          console.error("Signtool failed. Output: ");
-          console.error(err);
-          tl.setResult(tl.TaskResult.Failed, err.message);
-        } else {
-          console.log("Signtool succeeded. Output: ");
-          console.log(data);
-        }
-        console.log("Deleting securfile");
-        console.log("Job Finished: Successfully signed file " + filePath + " with certificate in " + secureFilePath);
-      });
+    glob(filePath, (err, files) => {
+      if (err) {
+        console.error("Resolving glob failed.");
+        console.error(err);
+        throw err;
+      }
+
+      files.forEach(filePath => sign(filePath, hashingAlgorithm, timeServer, secureFilePath, signCertPassword));
+    });
+
   } catch (err) {
     console.error(err);
     secureFileHelpers.deleteSecureFile();
     tl.setResult(tl.TaskResult.Failed, err);
   }
+}
+
+function sign(filePath: string, hashingAlgorithm: string, timeServer: string, secureFilePath: string, signCertPassword: string) : void {  
+  var exePath: string = path.resolve(__dirname, "./signtool.exe");
+  console.log("Executing signtool at " + exePath + " for file " + filePath + " and certificate " + secureFilePath);
+
+  exec.execFile(
+    exePath, 
+    ["sign", "/fd", hashingAlgorithm, "/t", timeServer, "/f", secureFilePath, "/p", signCertPassword, filePath],
+    (err, data) => {
+      if (err) {
+        console.error("Signtool failed. Output: ");
+        console.error(err);
+
+        throw err.message;
+      }
+
+      console.log("Signtool succeeded. Output: ");
+      console.log(data);      
+    });
 }
 
 run();
